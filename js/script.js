fc444d55e8cd490e44cc92aeba9dd81b0c9335d5
Jopee Zapido <jopeezapido@users.noreply.github.com>
2017-09-25 21:54:43 +0800

    modify structure

--- js/script.js
+++ js/script.js
@@ -1,4 +1,27 @@
 $(document).ready(function() {
-    var language= "en";
     
-})
+   function geoFindMe() {  
+ 
+         function supportCheck() {
+             if(!navigator.geolocation) {
+                 $('#box').html("Location not read by the browser");
+             }
+         };
+         
+         function success(position) {
+             var latitude = position.coords.latitude;
+             var longitude = position.coords.longitude;
+            $('#box').html("Latitude: " + latitude + "<br>Longitude: " + longitude)
+         };
+ 
+         function error() {
+             $('#box').html('Unable to get location.');
+         };
+ 
+         navigator.geolocation.getCurrentPosition(success, error);
+         
+    }//end of geoFindMe function
+
+    geoFindMe();
+
+});//end of document.ready
