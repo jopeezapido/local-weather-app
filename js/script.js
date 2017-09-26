d284b53dfffb6918618e45cb20557e955d2a8801
Jopee Zapido <jopeezapido@users.noreply.github.com>
2017-09-27 00:06:55 +0800

    return api call url

--- js/script.js
+++ js/script.js
@@ -9,9 +9,12 @@ $(document).ready(function() {
          };
          
          function success(position) {
-             var latitude = position.coords.latitude;
-             var longitude = position.coords.longitude;
-            $('#box').html("Latitude: " + latitude + "<br>Longitude: " + longitude)
+             var lat = position.coords.latitude;
+             var lon = position.coords.longitude;
+             var apiKey = "b38013bebe4a08f776f9ba995d7693b0";
+             var url = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
+             console.log(url);
+             return url;
          };
  
          function error() {
