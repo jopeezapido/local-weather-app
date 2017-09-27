b59ba153b96cd20e2ef00c51aba4faece7fd87dc
Jopee Zapido <jopeezapido@users.noreply.github.com>
2017-09-27 10:21:56 +0800

    refactor code

--- js/script.js
+++ js/script.js
@@ -1,30 +1,31 @@
 $(document).ready(function() {
-    
-   function geoFindMe() {  
- 
-         function supportCheck() {
-             if(!navigator.geolocation) {
-                 $('#box').html("Location not read by the browser");
-             }
-         };
-         
-         function success(position) {
-             var lat = position.coords.latitude;
-             var lon = position.coords.longitude;
-             var apiKey = "b38013bebe4a08f776f9ba995d7693b0";
-             var url = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
-             console.log(url);
-             return url;
-         };
- 
-         function error() {
-             $('#box').html('Unable to get location.');
-         };
- 
-         navigator.geolocation.getCurrentPosition(success, error);
-         
-    }//end of geoFindMe function
+    /*$ajax({
+        url: ,
+        dataType: JSON,
+        success: geoFindMe()
 
-    geoFindMe();
+    })*/
+
+    function getCoords() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(success, error);
+        } else {
+            alert("Your browser does not support geolocation.");
+        }
+    }
+
+    function success(position) {
+        var lat = position.coords.latitude;
+        var lon = position.coords.longitude;
+        var apiKey = "b38013bebe4a08f776f9ba995d7693b0";
+        var url = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;console.log(url);
+        return url;
+     };
+
+     function error() {
+         alert('Unable to retriev location.')
+     }
+
+     getCoords();
 
 });//end of document.ready
