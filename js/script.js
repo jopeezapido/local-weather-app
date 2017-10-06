70c58f326483e1d88e46052fb4ffc723772347e8
Jopee Zapido <jopeezapido@users.noreply.github.com>
2017-10-06 08:42:36 +0800

    refactor code using openweathermap API

--- js/script.js
+++ js/script.js
@@ -1,31 +1,35 @@
 $(document).ready(function() {
-    /*$ajax({
-        url: ,
-        dataType: JSON,
-        success: geoFindMe()
+    /*I need something that locates my location weherever my device is in*/
 
-    })*/
-
+    //check the browser support of the navigator object using the geolocation property
     function getCoords() {
-        if (navigator.geolocation) {
+
+        if(navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(success, error);
+            //check the if the search for the device's location is succesful or if not
+            function success(position) {
+                //get details for the url
+                var lat = position.coords.latitude;
+                var lon = position.coords.longitude;
+                var apiKey = "b38013bebe4a08f776f9ba995d7693b0";
+                var url = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
+                return url;
+            }
+
+            function error(positionError) {
+                alert("Unable to retrieve geolocation");
+            }
+
         } else {
-            alert("Your browser does not support geolocation.");
+            alert("Your browser does not support geolocation");
         }
-    }
 
-    function success(position) {
-        var lat = position.coords.latitude;
-        var lon = position.coords.longitude;
-        var apiKey = "b38013bebe4a08f776f9ba995d7693b0";
-        var url = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;console.log(url);
-        return url;
-     };
+    }//end of getCoords function
+     getCoords();//execute getCoords function to get the latitude and longitude of the location of the device, if successful.
 
-     function error() {
-         alert('Unable to retrieve location.')
-     }
+    /*2. I need something that gets the weather in the location mentioned in number 1.1*/
+    
 
-     getCoords();
+
 
 });//end of document.ready
