e8002771055062aebbb1372f1de1337f3703f314
Jopee Zapido <jopeezapido@users.noreply.github.com>
2017-10-07 12:34:55 +0800

    add ajax solution

--- js/script.js
+++ js/script.js
@@ -11,25 +11,29 @@ $(document).ready(function() {
                 //get details for the url
                 var lat = position.coords.latitude;
                 var lon = position.coords.longitude;
-                var apiKey = "b38013bebe4a08f776f9ba995d7693b0";
-                var url = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
+                var url = "https://fcc-weather-api.glitch.me/api/current?" + "lat=" + lat + "&lon=" + lon;
+                console.log(url);
                 return url;
-            }
-
+            };
+            
             function error(positionError) {
                 alert("Unable to retrieve geolocation");
-            }
+            };
 
         } else {
             alert("Your browser does not support geolocation");
+        };
+
+    };//end of getCoords function
+    //getCoords();//execute getCoords function to get the latitude and longitude of the location of the device, if successful.
+
+    /*1.2 I need something that gets the weather in the location mentioned in number 1.1*/
+    var url = getCoords();
+    $.ajax(url, {
+        success: function() {
+            //handle the JSON data here
+            console.log(main.temp);
         }
-
-    }//end of getCoords function
-     getCoords();//execute getCoords function to get the latitude and longitude of the location of the device, if successful.
-
-    /*2. I need something that gets the weather in the location mentioned in number 1.1*/
-    
-
-
+    });
 
 });//end of document.ready
